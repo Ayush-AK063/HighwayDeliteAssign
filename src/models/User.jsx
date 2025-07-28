a471288f4b0e545a459652321428944857a8ac5d
Ayush-AK063 <ayushmartandas@gmail.com>
2025-07-28 22:34:53 +0530

    Update User model

--- src/models/User.jsx
+++ src/models/User.jsx
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  dob: { type: Date, required: true },
   email: { type: String, required: true, unique: true },
   otp: String,
   otpExpiry: Date,
